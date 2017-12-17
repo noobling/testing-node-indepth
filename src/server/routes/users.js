478bf7e4673340709ec0dc57c4097da092c2e8b9
0x0 <ghstoreinvesting@gmail.com>
2017-12-17 20:20:22 +0800

    Add CRUD operations for users accompanying integration tests

---
+++ src/server/routes/users.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const router = express.Router();
+
+const knex = require('../db/knex');
+
+module.exports = router;
+
+router.get('/', (req, res, next) => {
+  knex('users').select('*')
+  .then((users) => {
+    res.status(200).json({
+      status: 'success',
+      data: users
+    });
+  })
+  .catch((err) => {
+    res.status(500).json({
+      status: 'error',
+      data: err
+    });
+  });
+});
+
+router.get('/:id', (req, res, next) => {
+  const userID = parseInt(req.params.id);
+  knex('users')
+    .select('*')
+    .where({
+      id: userID
+    })
+    .then((users) => {
+      res.status(200).json({
+        status: 'success',
+        data: users
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: 'error',
+        data: err
+      });
+    });
+});
+
+router.post('/', (req, res, next) => {
+  const newUsername = req.body.username;
+  const newEmail = req.body.email;
+  knex('users')
+  .insert({
+    username: newUsername,
+    email: newEmail
+  })
+  .returning('*')
+  .then((user) => {
+    res.status(201).json({
+      status: 'success',
+      data: user
+    });
+  })
+  .catch((err) => {
+    res.status(500).json({
+      status: 'error',
+      data: err
+    });
+  });
+});
+
+router.put('/:id', (req, res, next) => {
+  const userID = parseInt(req.params.id);
+  const updatedUsername = req.body.username;
+  const updatedEmail = req.body.email;
+  knex('users')
+  .update({
+    username: updatedUsername,
+    email: updatedEmail
+  })
+  .where({
+    id: userID
+  })
+  .returning('*')
+  .then((user) => {
+    res.status(200).json({
+      status: 'success',
+      data: user
+    });
+  })
+  .catch((err) => {
+    res.status(500).json({
+      status: 'error',
+      data: err
+    });
+  });
+});
+
+router.delete('/:id', (req, res, next) => {
+  const userID = parseInt(req.params.id);
+  knex('users')
+  .del()
+  .where({
+    id: userID
+  })
+  .returning('*')
+  .then((user) => {
+    res.status(200).json({
+      status: 'success',
+      data: user
+    });
+  })
+  .catch((err) => {
+    res.status(500).json({
+      status: 'error',
+      data: err
+    });
+  });
+});
